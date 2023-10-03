5383593d10bc83a27d2c2e33c8343f56bbb543b3
Lidia Gianne <lidiagianne@gmail.com>
2023-10-03 10:30:08 +0200

    block oak depth topics

--- src/dynamic_bridge.cpp
+++ src/dynamic_bridge.cpp
@@ -536,11 +536,25 @@ int main(int argc, char * argv[])
             }
             // printf("*** topic '%s' comparing '%s'\n", topic_name.c_str(), topic_name == "/kingfisher/motor_speed/0" ? "true" : "false");
             // printf("*** node '%s' comparing '%s'\n", node_name.c_str(), node_name == "/kingfisher/motor_speed/0" ? "true" : "false");
-            if (topic_name == "/kingfisher/motor_speed/0" || topic_name == "/kingfisher/motor_speed/1" || topic_name == "/kingfisher/motor_speed/2" || topic_name == "/kingfisher/motor_speed/3"){
+            //if (topic_name == "/kingfisher/motor_speed/0" || topic_name == "/kingfisher/motor_speed/1" || topic_name == "/kingfisher/motor_speed/2" || topic_name == "/kingfisher/motor_speed/3"){
 
-            } else{
-              active_publishers.insert(topic_name);
-              break;
+            //} else{
+             // active_publishers.insert(topic_name);
+             // break;
+            //}
+            const std::vector<std::string> invalid_topics = {
+                "/kingfisher/motor_speed/0", "/kingfisher/motor_speed/1", "/kingfisher/motor_speed/2", "/kingfisher/motor_speed/3",
+                "/oak/rgb/camera_info", "/oak/rgb/image_raw", "/oak/rgb/image_raw/compressed", "/oak/rgb/image_raw/compressedDepth",
+                "/oak/rgb/image_raw/theora", "/oak/right/image_rect", "/oak/right/image_rect/compressed", "/oak/right/image_rect/compressedDepth",
+                "/oak/right/image_rect/theora", "/oak/stereo/image_raw", "/oak/stereo/image_raw/compressed", "/oak/stereo/image_raw/compressedDepth",
+                "/oak/stereo/image_raw/theora"
+            };
+            
+            if (std::find(invalid_topics.begin(), invalid_topics.end(), topic_name) != invalid_topics.end()) {
+                
+            } else {
+                active_publishers.insert(topic_name);
+                break;
             }
             
           }
